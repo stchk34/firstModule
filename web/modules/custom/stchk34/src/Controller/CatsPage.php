0e9c0e517e784a6525a0e5b7cb95f6336fc0c84e
stchk34 <sandratkachukk@gmail.com>
2022-05-26 14:56:00 +0300

    insert data in db

--- web/modules/custom/stchk34/src/Controller/CatsPage.php
+++ web/modules/custom/stchk34/src/Controller/CatsPage.php
@@ -1,28 +1,27 @@
 <?php
 
+namespace Drupal\stchk34\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
 /**
- * @file 
+ * Provides route for our custom module.
  */
+class CatsPage extends ControllerBase {
 
- namespace Drupal\stchk34\Controller;
-
- use Drupal\Core\Controller\ControllerBase;
-
- /**
-  * Provides route for our custom module
-  */
-  class CatsPage extends ControllerBase {
- 
-    /**
-     * Display simple page
-     */
-    public function content():array {
-        $form = \Drupal::formBuilder()->getForm('Drupal\stchk34\Form\CatsForm');
-        $build['content'] =[
-          '#markup'=> t('Hello! You can add here a photo of your cat.'),
-          '#form' => $form,
-        ];
-        return $build;
-    }
+  /**
+   * Display simple page.
+   *
+   * @return array
+   *   Input content
+   */
+  public function content() {
+    $form = $this->formBuilder()->getForm('Drupal\stchk34\Form\CatsForm');
+    $build['content'] = [
+      '#markup' => $this->t('Hello! You can add here a photo of your cat.'),
+      '#form' => $form,
+    ];
+    return $build;
   }
- 
+
+}
--- web/modules/custom/stchk34/src/Form/CatsForm.php
+++ web/modules/custom/stchk34/src/Form/CatsForm.php
@@ -2,125 +2,161 @@
 
 namespace Drupal\stchk34\Form;
 
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\MessageCommand;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Ajax\AjaxResponse;
-use Drupal\Core\Ajax\CssCommand;
-use Drupal\Core\Ajax\HtmlCommand;
-use Drupal\Core\Ajax\MessageCommand;
-
- /**
- * Our simple form class.
- * 
- *  @package Drupal\stchk34\Form
- */
-class CatsForm extends ConfigFormBase{
-   public function getFormId() {
-     return 'stchk34_settings';
-    }
-
-   /**
-   * {@inheritdoc}
-   */
-   public function getEditableConfigNames() {
-     return ['stchk34.settings'];
-   }
- 
-   /**
-   * {@inheritdoc}
-   */
-   public function buildForm(array $form, FormStateInterface $form_state) {
-      $form['cats_name'] = [
-        '#type' => 'textfield',
-        '#title' => t('Your cat’s name:'),
-        '#description'=>t('the minimum length of the name-2 and the maximum-32'),
-        '#required' => TRUE,
-      ];
-
-      $form['email'] = [
-        '#type' => 'email',
-        '#title' => t('Your email:'),
-        '#required' => TRUE,
-        '#description' => t('Email names can only contain Latin letters, underscores, or hyphens'),
-        '#maxlength' => 25,
-        '#ajax' => [
-          'callback' => '::validateEmailAjax',
-          'event' => 'keyup',
-        ],
-      ];
-
-      $form['cat_image'] = [
-        '#type' => 'managed_file',
-        '#title' => $this->t('Your cat’s image:'),
-        '#required' => true,
-        '#description' => $this->t('Valid extensions: jpeg, jpg, png. Max file size 2MB'),
-        '#multiple' => FALSE,
-        '#default_value' => $this->config('stchk34.settings')->get('cat_image'),
-        '#upload_location' => 'public://stchk34/cats',
-        '#upload_validators' => [
-          'file_validate_extensions' => ['jpeg jpg png'],
-          'file_validate_size' => [2100000]
-        ],
-      ];
-
-      $form['actions']['#type'] = 'actions';
-      $form['actions']['submit'] = [
-        '#type' => 'submit',
-        '#value' => t('Add cat'),
-        '#button_type' => 'primary',
-        '#ajax' => [
-          'callback' => '::ajaxSubmit',
-        ],
-      ];
-      return $form;
-    }
-
-     /**
-  * Validate form
-  */
-  public function validateForm(array &$form, FormStateInterface $form_state)
-  {
-    $email=$form_state->getValue('email');
-      if (strlen($form_state->getValue('cats_name')) < 2) {
-        $form_state->setErrorByName('cats_name', $this->t('Name is too short.'));
-      } elseif (strlen($form_state->getValue('cats_name')) >32) {
-        $form_state->setErrorByName('cats_name', $this->t('Name is too long.'));
-      }
-      if((!filter_var($email, FILTER_VALIDATE_EMAIL)) || (strpbrk($email,$stableExpression))){
-        $form_state->setErrorByName('cats_name', $this->t('Invalid email'));
-      }
-  }
+use Drupal\file\Entity\File;
 
 /**
-* {@inheritdoc}
-*/
-  public function ajaxSubmit(array $form, FormStateInterface $form_state)
-  {
+ * Our simple form class.
+ *
+ * @package Drupal\stchk34\Form
+ */
+class CatsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'stchk34_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getEditableConfigNames() {
+    return ['stchk34.settings'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['cats_name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Your cat’s name:'),
+      '#description' => $this->t('the minimum length of the name-2 and the maximum-32'),
+      '#required' => TRUE,
+    ];
+
+    $form['email'] = [
+      '#type' => 'email',
+      '#title' => $this->t('Your email:'),
+      '#required' => TRUE,
+      '#description' => $this->t('Email names can only contain Latin letters, underscores, or hyphens'),
+      '#maxlength' => 25,
+      '#ajax' => [
+        'callback' => '::validateEmailAjax',
+        'event' => 'keyup',
+      ],
+    ];
+
+    $form['cat_picture'] = [
+      '#type' => 'managed_file',
+      '#title' => $this->t('Your cat’s image:'),
+      '#required' => TRUE,
+      '#description' => $this->t('Valid extensions: jpeg, jpg, png. Max file size 2MB'),
+      '#multiple' => FALSE,
+      '#default_value' => $this->config('stchk34.settings')->get('cat_picture'),
+      '#upload_location' => 'public://stchk34/cats',
+      '#upload_validators' => [
+        'file_validate_extensions' => ['jpeg jpg png'],
+        'file_validate_size' => [2100000],
+      ],
+    ];
+
+    $form['actions']['#type'] = 'actions';
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Add cat'),
+      '#button_type' => 'primary',
+      '#ajax' => [
+        'callback' => '::ajaxSubmit',
+      ],
+    ];
+    return $form;
+  }
+
+  /**
+   * Validate form.
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    if (strlen($form_state->getValue('cats_name')) < 2) {
+      $form_state->setErrorByName('cats_name', $this->t('Name is too short.'));
+    }
+    elseif (strlen($form_state->getValue('cats_name')) > 32) {
+      $form_state->setErrorByName('cats_name', $this->t('Name is too long.'));
+    }
+  }
+
+  /**
+   * @return bool
+   *   email validation.
+   */
+  protected function validateEmail(array &$form, FormStateInterface $form_state) {
+    $email = $form_state->getValue('email');
+    $stableExpression = '/^[A-Za-z_\-]+@\w+(?:\.\w+)+$/';
+    if (preg_match($stableExpression, $email)) {
+      return TRUE;
+    }
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $picture = $form_state->getValue('cat_picture');
+    $data = [
+      'cats_name' => $form_state->getValue('cats_name'),
+      'email' => $form_state->getValue('email'),
+      'timestamp' => time(),
+      'cat_image' => $picture[0],
+    ];
+
+    $file = File::load($picture[0]);
+    $file->setPermanent();
+    $file->save();
+
+    \Drupal::database()->insert('stchk34')->fields($data)->execute();
+  }
+
+  /**
+   * @return \Drupal\Core\Ajax\AjaxResponse
+   *
+   *   Validation Email.
+   */
+  public function ajaxSubmit(array $form, FormStateInterface $form_state) {
     $response = new AjaxResponse();
-    if ($form_state->hasAnyErrors()) {
+    $valid = $this->validateEmail($form, $form_state);
+    if ($form_state->hasAnyErrors() || !$valid) {
       foreach ($form_state->getErrors() as $errors_array) {
         $response->addCommand(new MessageCommand($errors_array));
       }
-    } else {
-        $response->addCommand(new MessageCommand('You adedd a cat!'));
     }
-    \Drupal::messenger()->deleteAll();
+    else {
+      $response->addCommand(new MessageCommand('You added a cat!'));
+    }
+    $this->messenger()->deleteAll();
     return $response;
   }
 
-/**
-* {@inheritdoc}
-*/
-  public function validateEmailAjax(array &$form, FormStateInterface $form_state)
-  {
+  /**
+   * @return \Drupal\Core\Ajax\AjaxResponse
+   *
+   *   Validation Email.
+   */
+  public function validateEmailAjax(array &$form, FormStateInterface $form_state): AjaxResponse {
+    $valid = $this->validateEmail($form, $form_state);
     $response = new AjaxResponse();
-    $email=$form_state->getValue('email');
-    $stableExpression = '/^[A-Za-z_\-]+@\w+(?:\.\w+)+$/';
-    if(!strpbrk($email,$stableExpression)){
+    if (!$valid) {
       $response->addCommand(new MessageCommand('Invalid Email'));
-    } else{
-        $response->addCommand(new MessageCommand('',".null",[],true));
+    }
+    else {
+      $response->addCommand(new MessageCommand('', ".null", [], TRUE));
     }
     return $response;
   }
+
 }
