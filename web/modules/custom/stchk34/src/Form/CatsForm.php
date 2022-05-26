9c34fcc2acd99f710d14bed2160118dc58b2a20f
stchk34 <sandratkachukk@gmail.com>
2022-05-26 03:27:32 +0300

    add img field

--- web/modules/custom/stchk34/src/Form/CatsForm.php
+++ web/modules/custom/stchk34/src/Form/CatsForm.php
@@ -22,22 +22,21 @@ class CatsForm extends ConfigFormBase{
    /**
    * {@inheritdoc}
    */
-  public function getEditableConfigNames() {
-    return ['stchk34.settings'];
-  }
+   public function getEditableConfigNames() {
+     return ['stchk34.settings'];
+   }
  
    /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state)
-  {
-
+   public function buildForm(array $form, FormStateInterface $form_state) {
       $form['cats_name'] = [
         '#type' => 'textfield',
         '#title' => t('Your cat’s name:'),
         '#description'=>t('the minimum length of the name-2 and the maximum-32'),
         '#required' => TRUE,
       ];
+
       $form['email'] = [
         '#type' => 'email',
         '#title' => t('Your email:'),
@@ -49,6 +48,21 @@ class CatsForm extends ConfigFormBase{
           'event' => 'keyup',
         ],
       ];
+
+      $form['cat_image'] = [
+        '#type' => 'managed_file',
+        '#title' => $this->t('Your cat’s image:'),
+        '#required' => true,
+        '#description' => $this->t('Valid extensions: jpeg, jpg, png. Max file size 2MB'),
+        '#multiple' => FALSE,
+        '#default_value' => $this->config('stchk34.settings')->get('cat_image'),
+        '#upload_location' => 'public://stchk34/cats',
+        '#upload_validators' => [
+          'file_validate_extensions' => ['jpeg jpg png'],
+          'file_validate_size' => [2100000]
+        ],
+      ];
+
       $form['actions']['#type'] = 'actions';
       $form['actions']['submit'] = [
         '#type' => 'submit',
@@ -59,55 +73,55 @@ class CatsForm extends ConfigFormBase{
         ],
       ];
       return $form;
-  }
+    }
 
   /**
   * Validate form
   */
-  public function validateForm(array &$form, FormStateInterface $form_state)
-  {
-    $email=$form_state->getValue('email');
-      if (strlen($form_state->getValue('cats_name')) < 2) {
+    public function validateForm(array &$form, FormStateInterface $form_state)
+    {
+      $email=$form_state->getValue('email');
+        if (strlen($form_state->getValue('cats_name')) < 2) {
           $form_state->setErrorByName('cats_name', $this->t('Name is too short.'));
-      } elseif (strlen($form_state->getValue('cats_name')) >32) {
+        } elseif (strlen($form_state->getValue('cats_name')) >32) {
           $form_state->setErrorByName('cats_name', $this->t('Name is too long.'));
-      }
-      if((!filter_var($email, FILTER_VALIDATE_EMAIL)) || (strpbrk($email,$stableExpression))){
-        $form_state->setErrorByName('cats_name', $this->t('Invalid email'));
-      }
-  }
+        }
+        if((!filter_var($email, FILTER_VALIDATE_EMAIL)) || (strpbrk($email,$stableExpression))){
+          $form_state->setErrorByName('cats_name', $this->t('Invalid email'));
+        }
+    }
 
   /**
   * {@inheritdoc}
   */
-  public function ajaxSubmit(array $form, FormStateInterface $form_state)
-  {
+    public function ajaxSubmit(array $form, FormStateInterface $form_state)
+    {
       $response = new AjaxResponse();
       if ($form_state->hasAnyErrors()) {
-          foreach ($form_state->getErrors() as $errors_array) {
-              $response->addCommand(new MessageCommand($errors_array));
-          }
+        foreach ($form_state->getErrors() as $errors_array) {
+          $response->addCommand(new MessageCommand($errors_array));
+        }
       } else {
-            $response->addCommand(new MessageCommand('You adedd a cat!'));
+          $response->addCommand(new MessageCommand('You adedd a cat!'));
       }
       \Drupal::messenger()->deleteAll();
       return $response;
-  }
+    }
 
   /**
   * {@inheritdoc}
   */
-  public function validateEmailAjax(array &$form, FormStateInterface $form_state)
-  {
+    public function validateEmailAjax(array &$form, FormStateInterface $form_state)
+    {
       $response = new AjaxResponse();
       $email=$form_state->getValue('email');
       $stableExpression = '/[^A-Za-z_\-]+@\w+(?:\.\w+)+$/';
       if(strpbrk($email,$stableExpression)){
-          $response->addCommand(new MessageCommand('Invalid Email'));
+        $response->addCommand(new MessageCommand('Invalid Email'));
       } else{
           $response->addCommand(new MessageCommand('',".null",[],true));
       }
       return $response;
-  }
+    }
 
 }
